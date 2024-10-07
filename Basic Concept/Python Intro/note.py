5258351c9bfb030ccb73d31a3a9278072c233e5f
AnugrahaTamang <anugrah59@gmail.com>
2024-10-07 18:17:39 +0545

    upadate

---
+++ Basic Concept/Python Intro/note.py
@@ -0,0 +1,4 @@
+#Python is a popular programming language. which is used to server side,mathematics,data sciece.
+#It's developed by Guido Van Rossum and released 1991.
+#first program
+print("Hello I am Anugraha Tamang")
---
+++ Basic Concept/variable/note.py
@@ -0,0 +1,8 @@
+#variable is container which is used to store the different type of data.It's also known as memory location.
+name = "anugraha tamang"
+print(name)
+value = 12
+Value = 254
+print(value)
+print(Value) #case sensitive programming language.
+
