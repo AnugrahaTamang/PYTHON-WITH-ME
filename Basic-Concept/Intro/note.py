efebaab4ad28d26aea63ff088d0e4b226863ac27
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-10-19 08:56:32 +0545

    intro

---
+++ Basic-Concept/Intro/note.py
@@ -0,0 +1,5 @@
+#Python is popular programming language which is developed by Guido Van Rossum in 1991.
+#simple python program
+
+print("Python is one of the most popular programming in the world")
+print("Hello python")
