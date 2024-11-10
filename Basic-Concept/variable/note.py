e6f586ce292d04293f3e152228d1f86783c55fbf
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-11-10 21:18:20 +0545

    variable

---
+++ Basic-Concept/variable/note.py
@@ -0,0 +1,15 @@
+# vriable is container which is used to store different type of data value.
+a = 12
+b = 13
+print(a+b)
+a = "Anugraha Tamang"
+print(a)
+
+#Two types of variable in python .
+#1. Mutable: which is changeable.
+#2. Immutable : which is unchangeable.
+
+print(2+4j) #Complex variable
+
+a = True
+print(a)
