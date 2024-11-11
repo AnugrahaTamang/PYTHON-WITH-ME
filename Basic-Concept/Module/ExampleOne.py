ba3117ccc4dfe57662468392f893fd226b085185
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-11-11 23:21:25 +0545

    module

---
+++ Basic-Concept/Module/ExampleOne.py
@@ -0,0 +1,11 @@
+from note import Intro, add
+Intro("Anugraha Tamang", 22)
+
+print(add(22, 24))
+
+#Built-in Module
+import math
+print(math.floor(12.222))
+print(math.factorial(5))
+
+#External Modules(libraries): numpy, pandas, requests, pip
---
+++ Basic-Concept/Module/note.py
@@ -0,0 +1,7 @@
+# module is already written code . Two Types of module : 1. Default Module 2. Built-in Module
+def Intro(name, age):
+    print(f"My name is {name} . I am {age} years old")
+
+def add(a, b):
+    return a + b
+
