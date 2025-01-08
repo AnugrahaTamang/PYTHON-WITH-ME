4bb7a98a4d2bc0e5d8bde2b4211e8a16673a8012
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-01-08 18:02:28 +0545

    tuples

---
+++ Basic-Concept/Tuple/ExampleOne.py
@@ -0,0 +1,12 @@
+name = ("ramit", "jeewan", "milan")
+print(name)
+print(name.count("ramit"))
+print(name[2])
+for x in name:
+    print(x)
+num = (11,12,4,32,45,5)
+print(min(num))
+print(max(num))
+a, b, c = ("ram", "bam", "prwm")
+print(a)
+
