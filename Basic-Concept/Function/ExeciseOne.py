fd4dcf3bc9cb0388967f3efe00c6bc08f1901a00
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-01-08 11:52:55 +0545

    function

---
+++ Basic-Concept/Function/ExeciseOne.py
@@ -0,0 +1,23 @@
+x = "awesome"
+def myfun():
+    x = "fantastic"
+    print("Python is", x)
+myfun()
+
+print("Python is", x)
+'''
+number1 = int(input("Enter yuor Number: "))
+number2 = int(input("Enter your number: "))
+number3 = int(input("Enter your number: "))
+
+minimum = number1
+if number2 < minimum:
+    minimum = number2
+
+if number3 < minimum:
+    minimum = number3
+
+print("Minimum value is ", minimum)'''
+
+print(max(12,13,14))
+print(min(12,13,14))
