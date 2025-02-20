bac9179dea3bf0f7500b11b1308efdd77622ac07
AnugrahaTamang <tamanganugrah59@gmail.com>
2025-02-20 19:11:51 +0545

    canvas

---
+++ Advance Concept/Canvas_in_python/Example.py
@@ -0,0 +1,11 @@
+from tkinter import *
+win = Tk()
+canvas = Canvas(win, height=500, width=500)
+canvas.create_line(0,0,500,500,fill="blue", width=5)
+canvas.create_line(0,500,500,0,fil="gray",width=10)
+canvas.create_rectangle(50,50,250,250,fill="purple",width=8)
+canvas.create_polygon(250,0,500,500,0,500)
+canvas.create_arc(0,0,500,500,fill="red", extent=180,width=10)
+canvas.create_oval(190,190,310,310,fill="yellow")
+canvas.pack()
+win.mainloop()
